567e3479361329b4b912c2a406c7e8d5e99d1bab
Rayaan <rayanafarmaa18@gmail.com>
2020-10-19 10:37:50 +0530

    my first commit

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,9 +1,31 @@
-import React,{ useState,useContext} from 'react';
+import React,{ useState,useContext,useEffect} from 'react';
 import ContactContext from '../../context/contact/contactContext';
 
 export const ContactForm = () => {
 
     const contactContext = useContext(ContactContext);
+
+    const { addContact,updateContact,current,clearCurrent} = contactContext;
+
+    useEffect(()=>{
+        if(current !==null){
+
+            setContact(current);
+        }
+        else{
+
+            setContact({
+      
+                name:'',
+                email:'',
+                phone:'',
+                type:'personal'
+        
+                });
+
+        }
+
+    },[contactContext,current])
 
     const[contact,setContact] = useState( {
       
@@ -17,26 +39,44 @@ export const ContactForm = () => {
     const{  name,email,phone,type} = contact;
 
     const onChange = e =>
-    setContact({...contact,[e.target.name]:[e.target.value]});
+    setContact({...contact,[e.target.name]: e.target.value });
 
     const onSubmit = e => {
         e.preventDefault();
-        contactContext.addContact(contact);
 
-        setContact({
+        if(current ===null){
+            addContact(contact);
+
+        }
+        else{
+            updateContact(contact);
+            clearCurrent();
+
+        }
       
-        name:'',
-        email:'',
-        phone:'',
-        type:'personal'
 
-        })
+        // setContact({
+      
+        // name:'',
+        // email:'',
+        // phone:'',
+        // type:'personal'
+
+        // });
+    };
+
+    const clearAll = () => {
+
+        clearCurrent();
+
+
+
     }
 
     return (
 
       <form onSubmit={onSubmit}>
-          <h2 className="text-primary">Add Contact</h2>
+          <h2 className="text-primary">{ current ? 'Edit Contact':'Add Contact'   }</h2>
           <input type="text" name="name" placeholder="Name" value={name} onChange={onChange}/>
           <input type="email" name="email" placeholder="Email" value={email} onChange={onChange}/>
           <input type="text" name="phone" placeholder="Phone"value={phone} onChange={onChange}/>
@@ -44,8 +84,16 @@ export const ContactForm = () => {
           <input type="radio" name="type" value='personal'checked={type === 'personal'}onChange={onChange}/> personal {''}
           <input type="radio" name="type" value= 'professional'checked={type ==='professional'}onChange={onChange}/> professional {''}
           <div>
-              <input type="submit" value="Add Contact" className="btn btn-block btn-primary"/>
+              <input type="submit" value= {current ? 'Update Contact':'Add Contact'} className="btn btn-block btn-primary"/>
           </div>
+          {current && (
+              <div>
+                  <button className="btn btn-light btn-block"onClick={clearAll}>Clear</button>
+              </div>
+
+          )
+
+          }
 
       </form>
     )
